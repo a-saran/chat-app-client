d4661bb86f0dafff55a526107163bd7052fa9b51
asaran97 <asaran234@gmail.com>
2020-04-25 19:20:26 +0530

    added initial components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Chat from './components/chat'
+import Join from './components/join';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path='/' component={Join} exact/>
+      <Route path='/chat' component={Chat} exact/>
+    </Switch>
+  </Router>
+)
 
 export default App;

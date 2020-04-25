7c5f33737021ff40e26db8a0ffccb9e9eb099c41
asaran97 <asaran234@gmail.com>
2020-04-25 23:49:41 +0530

    added .env and basic setup -socket in chat component

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  let socket;
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(process.env.REACT_APP_BACKEND_URL)
+    console.log(socket)
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room }, (error) => {
+
+    })
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  }, [location.search])
+
   return (
     <div>
       <h1>Chat</h1>
